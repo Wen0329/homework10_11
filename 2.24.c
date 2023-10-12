f11b09ada1f872fa6776fbf4b8d7f6a54f54eed9
Wen0329 <yl260329@gmail.com>
2023-10-12 13:28:06 +0800

    Create 2.24.c

---
+++ 2.24.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int A;
+    printf("請輸入一個數");
+    scanf ("%d",&A );
+    if ((A%2)==0)
+    {printf ("A是偶數");
+    }
+    else
+    {printf("A是奇數");
+    }
+}
